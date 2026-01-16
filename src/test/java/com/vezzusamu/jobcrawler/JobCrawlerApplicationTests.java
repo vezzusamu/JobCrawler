75698c80658c72dcac5d01d4cef51d1833871b5e
vezzusamu <svezzuto97@gmail.com>
2026-01-16 12:42:12 +0100

    Project Initialized

---
+++ src/test/java/com/vezzusamu/jobcrawler/JobCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vezzusamu.jobcrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
